d8dcb57ead016691c1d590b2457f7794f3a612a2
Chuck <firstinmath2@gmail.com>
2023-05-30 16:12:40 -0400

    Now able to handle multiple classes

--- course_checker.py
+++ course_checker.py
@@ -1,10 +1,9 @@
 import requests
-import json
 
 class_inputs = []
 
 keep_going = 0
-while keep_going == 1:
+while keep_going == 0:
 
     user_input = input("Please Enter the class you want to track \nPlease make sure that you enter the class code in all CAPS for example ENGL101\n")
     second_user_input = input("Do you want to enter another class \n---> yes or no?\n")
@@ -14,17 +13,24 @@ while keep_going == 1:
     if second_user_input !=  "yes":
         keep_going = 1
 
-parameters ={ 
-    "course_id": "CMSC351",
-}
+def print_data():
 
-
-data = requests.get('https://api.umd.io/v1/courses/sections', params=parameters).json()
-
-for section_data in data:
-    if(len(section_data.get('instructors')) == 0):
-        print("TBA")
-    else:
-        print(section_data.get('instructors'))
+	for user_input_data in class_inputs:
+		parameters ={ 
+    	"course_id": user_input_data,   
+		}
         
-    print(section_data.get('open_seats'))
+		data = requests.get('https://api.umd.io/v1/courses/sections', params=parameters).json()
+
+		for section_data in data:
+			if(len(section_data.get('instructors')) == 0):
+				print("TBA")
+			else:
+				print(section_data.get('instructors'))
+
+			print(section_data.get('open_seats'))
+
+
+print_data()
+					
+	
