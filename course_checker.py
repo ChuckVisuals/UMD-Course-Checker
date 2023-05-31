944a38e167ccfec5e520180c0393906f39e3e9af
Chuck <firstinmath2@gmail.com>
2023-05-30 22:11:58 -0400

    Fixed bug

--- course_checker.py
+++ course_checker.py
@@ -1,34 +1,55 @@
 import requests
 
+
+# Class Object used to store the data for the course data
+class CourseData(object):
+	id = ""
+	class_name = ""
+
+	def __init__ (self,id, class_name):
+		self.id = id
+		self.class_name = class_name
+
+
 class_inputs = []
+
 
 keep_going = 0
 while keep_going == 0:
 
     user_input = input("Please Enter the class you want to track \nPlease make sure that you enter the class code in all CAPS for example ENGL101\n")
+    user_section_input = input("Please Enter the section number for the class you want to track\n")
     second_user_input = input("Do you want to enter another class \n---> yes or no?\n")
+    
+    course_data = CourseData(user_section_input,user_input)
 
-    class_inputs.append(user_input)
+    class_inputs.append(course_data)
 
     if second_user_input !=  "yes":
         keep_going = 1
 
 def print_data():
 
-	for user_input_data in class_inputs:
-		parameters ={ 
-    	"course_id": user_input_data,   
-		}
+	# Processes each CourseData Object
+	for course_data in class_inputs:
+	
+
+		print(course_data.class_name + '-' + course_data.id)
         
-		data = requests.get('https://api.umd.io/v1/courses/sections', params=parameters).json()
+		data = requests.get('https://api.umd.io/v1/courses/sections/' + course_data.class_name + '-' + course_data.id).json()
 
+
+		# Processes and print out the course data
 		for section_data in data:
-			if(len(section_data.get('instructors')) == 0):
-				print("TBA")
-			else:
-				print(section_data.get('instructors'))
 
-			print(section_data.get('open_seats'))
+			# Checks to see if the instructor is a TBA
+			if(len(section_data.get('instructors')) == 0):
+				print("Instructor: TBA")
+			else:
+				print("Instructor:" + section_data.get('instructors'))
+
+			# Prints out the seat count
+			print("Seats left:" + section_data.get('open_seats'))
 
 
 print_data()
