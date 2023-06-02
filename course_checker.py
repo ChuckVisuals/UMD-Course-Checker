65e4bdc27d6bf096139876ffff436007fd916d13
Chuck <firstinmath2@gmail.com>
2023-06-02 00:14:28 -0400

    Added SMS support and hid the keys hopefully

--- course_checker.py
+++ course_checker.py
@@ -1,5 +1,7 @@
 import requests
-
+from twilio.rest import Client
+import time
+import json
 
 # Class Object used to store the data for the course data
 class CourseData(object):
@@ -10,16 +12,37 @@ class CourseData(object):
 		self.id = id
 		self.class_name = class_name
 
+ 
 
+# Specify the path to your JSON file
+file_path = r'C:\Users\first\Desktop\New folder\UMD-Course-Checker\keys.json'
+
+# Open the JSON file and load its contents
+with open(file_path, 'r') as file:
+    key_data = json.load(file)
+    
+with open(file_path, 'r') as file:
+    account_sid_data = json.load(file)
+    
+# SMS Information
+account_sid = account_sid_data['sid']
+auth_token = key_data['key']
+client = Client(account_sid, auth_token)
+
+
+# Global Variables
 class_inputs = []
+seat_count = 0
 
 # While loop used to process user input 
 keep_going = 0
 while keep_going == 0:
-
-    user_input = input("Please Enter the class you want to track \nMake sure that you enter the class code in all CAPS for example ENGL101\n")
-    user_section_input = input("Please Enter the section number for the class you want to track\n")
-    second_user_input = input("Do you want to enter another class \n---> yes or no?\n")
+    
+    user_number = input("Please enter your number\n")
+    user_input = input("Please Enter the class you want to track \nMake sure that you enter the class code in all CAPS for example ENGL101\n\n")
+    user_section_input = input("Please Enter the section number for the class you want to track\n\n")
+    second_user_input = input("Do you want to enter another class \n---> yes or no?\n\n")
+    
     
     course_data = CourseData(user_section_input,user_input)
 
@@ -47,10 +70,39 @@ def print_data():
 				print(section_data.get('instructors'))
 
 			# Prints out the seat count
-			print("Seats left:" + section_data.get('open_seats'))
+			print("Seats left: " + section_data.get('open_seats') + "\n")
+			seat_count = section_data.get('open_seats')
 
 
 print_data()
+
+message = client.messages.create(
+				body="You have successfully set up your data in the script, you will receive a message when your class has seats available",
+				from_="+18334320310",
+				to="+1" + user_number
+				)
+
+# Used to Check if the seat count is greater then 0 it will send a SMS message to
+# the user
+while True:
+
+	for course_data in class_inputs:
+		# Pulls the data using a API
+		data = requests.get('https://api.umd.io/v1/courses/sections/' + course_data.class_name + '-' + course_data.id).json()
+
+		# Check to see if theres seats open for the course
+		for section_data in data:
+			if int(section_data.get('open_seats')) > 0:
+
+				message = client.messages.create(
+				body="Theres a seat available for " + course_data.class_name + " hurry and sign up!!!",
+				from_="+18334320310",
+				to="+1" + user_number
+				)
+
+				# Waits a day before checking again
+				time.sleep(86400)
+
 
 input("Enter Anything to close\n")
 					
