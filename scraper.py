a6b802ff382b5cc2ee4914baee381c9f958cf782
Chuck <firstinmath2@gmail.com>
2024-05-14 23:44:22 -0400

    started cron

--- scraper.py
+++ scraper.py
@@ -1,29 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url = "https://app.testudo.umd.edu/soc/search?courseId=CMSC351&sectionId=&termId=202308&_openSectionsOnly=on&creditCompare=%3E%3D&credits=0.0&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
+load_dotenv()
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+print(url, key)
+supabase: Client = create_client(url, key)
 
-result = requests.get(url)
-filtered_divs = []
+
+# gets the seat count from testudo for a specific class and section
+def get_seats(class_name, section_id):
+    url = f'https://app.testudo.umd.edu/soc/search?courseId={class_name.upper()}&sectionId=&termId=202401&_openSectionsOnly=on&creditCompare=%3E%3D&credits=0.0&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
+
+    result = requests.get(url)
+    filtered_divs = []
+
+    doc = BeautifulSoup(result.text ,"html.parser")
+    divs = doc.find_all('div', class_='section delivery-f2f')
+
+    for div in divs:
+        span = div.find('span', class_='section-id')
+        if span and span.text.strip() == section_id:
+            filtered_divs.append(div)
+            seat_span = div.find('span', class_='open-seats-count')
+            if seat_span:
+                return int(seat_span.text.strip())
+            
+print(get_seats('CMSC131', '0101'))
 
 
-
-doc = BeautifulSoup(result.text ,"html.parser")
-#print(doc.prettify())
-#spam = doc.find("span",id="open-seats-count")
-#seats = doc.find_all(text="Open:")
-divs = doc.find_all('div', class_='section delivery-f2f')
-
-for div in divs:
-    span = div.find('span', class_='section-id')
-    if span and span.text.strip() == '0101':
-        filtered_divs.append(div)
-        seat_span = div.find('span', class_='open-seats-count')
-        if seat_span:
-            print((int(seat_span.text.strip())))
+def update_seats(class_name, section_id):
     
-for div in filtered_divs:
-    print(div.prettify())
-    print("---------------------------------------------------")
-    print("\n")
-print(len(filtered_divs))
+    seats = get_seats(class_name, section_id)
+    supabase.table('data').update({'open_seats': seats}).eq('class_name', class_name).execute()
+
+
+# Loop through each row in the database and update the seat count
+def update_all_seats():
+    # Fetch all rows from the database table
+    rows = supabase.table('data').select('*').execute()
+    
+    # Iterate through each row
+    for row in rows.data:
+        class_name = row['class_name']
+        section_id = row['section']
+        
+        # Update the seat count for the current row
+        update_seats(class_name, section_id)
+
+
+# Call the function to update all seats
+update_all_seats()
